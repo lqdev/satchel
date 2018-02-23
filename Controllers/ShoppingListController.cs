3ccdc47cee91adb90f7437e59c6fcf851d9397eb
lquinta5 <lquinta5@mail.depaul.edu>
2018-02-23 18:42:14 +0000

    completed CRUD operations

--- Controllers/ShoppingListController.cs
+++ Controllers/ShoppingListController.cs
@@ -29,5 +29,71 @@ namespace satchel.Controllers
 		{
 			return _context.ShoppingListItems.AsNoTracking().ToList();
 		}
+
+
+		[HttpGet("{id}",Name="GetItem")]
+		public IActionResult GetById(long id)
+		{
+			var item = _context.ShoppingListItems.FirstOrDefault(t => t.Id == id);
+
+			if(item == null)
+			{
+				return NotFound();
+			}
+
+			return new ObjectResult(item);
+		}
+
+		[HttpPost]
+		public IActionResult Create([FromBody] ShoppingListItem item)
+		{
+			if(item == null)
+			{
+				return BadRequest();
+			}
+
+			_context.ShoppingListItems.Add(item);
+			_context.SaveChanges();
+
+			return CreatedAtRoute("GetItem",new {id = item.Id},item);
+		}
+
+		[HttpPut("{id}")]
+		public IActionResult Update(long id, [FromBody] ShoppingListItem item)
+		{
+			if(item == null || item.Id != id)
+			{
+				return BadRequest();
+			}
+
+			var slitem = _context.ShoppingListItems.FirstOrDefault(t => t.Id == id);
+			if(slitem == null)
+			{
+				return NotFound();
+			}
+
+			slitem.Name = item.Name;
+			slitem.Purchased = item.Purchased;
+
+			_context.ShoppingListItems.Update(slitem);
+			_context.SaveChanges();
+
+			return new NoContentResult();
+		}
+
+		[HttpDelete("{id}")]
+		public IActionResult Delete(long id)
+		{
+			var slitem = _context.ShoppingListItems.FirstOrDefault(t => t.Id == id);
+			if(slitem == null)
+			{
+				return NotFound();
+			}
+
+			_context.ShoppingListItems.Remove(slitem);
+			_context.SaveChanges();
+
+			return new NoContentResult();
+		}
 	}
 }
