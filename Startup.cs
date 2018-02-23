93623b272c1d4f69fa554a3bccc9113e2cd62a8c
lquinta5 <lquinta5@mail.depaul.edu>
2018-02-23 20:33:32 +0000

    Added code, but requests break

--- Startup.cs
+++ Startup.cs
@@ -24,7 +24,7 @@ namespace satchel
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-	    services.AddDbContext<ShoppingListContext>(options => options.UseInMemoryDatabase("ShoppingList"));
+	    services.AddDbContext<ShoppingListContext>(options => options.UseSqlite("Data Source=shoppinglist.db"));
             services.AddMvc();
         }
 
